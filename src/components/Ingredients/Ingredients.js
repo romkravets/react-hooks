3fbac5e02539744d3fb047a49aec7da25f0ef3da
romkravets <romann.kravets@gmail.com>
2020-03-29 17:25:17 +0300

    Sending Http Requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,12 +8,23 @@ const Ingredients = () => {
 
   const [ userIngredients, setUserIngredients ] = useState([]);
 
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      {id: Math.random().toString(), ...ingredient}
-    ])
-  }
+    fetch('https://react-hooks-b5812.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify({ingredient}),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then( response => {
+      return  response.json();
+    })
+      .then( responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        {id: responseData.name, ...ingredient}
+      ]);
+    });
+  };
 
   return (
     <div className="App">
